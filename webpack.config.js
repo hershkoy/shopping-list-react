2a9efad03656e7b65f508d7634b9ccad735343c5
Hezi H <hershkoy@gmail.com>
2020-12-22 21:08:02 +0200

    reading style from file works!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './source/js/index.js',
@@ -17,15 +18,15 @@ module.exports = {
           presets: ['es2015', 'react']
         }
       },
-      {
-        test: /\.css$/,
-        loader: 'css-loader'
-      }  
+      { 
+        test: /\.css$/, 
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader") }  
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './source/index.ejs'
-    })
+    }),
+    new ExtractTextPlugin("styles.css")
   ]
 };
